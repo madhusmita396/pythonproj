4335146e20b284fab4e15259f7468e5df834d55f
smita4351 <prsneha2015@gmail.com>
2022-09-13 14:12:17 +0100

    first commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,53 @@
+from distutils.command.upload import upload
+from email.mime import image
+from tabnanny import verbose
 from django.db import models
+from django.urls import reverse
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from taggit.managers import TaggableManager
+
+# from django_resized import ResizedImageField
+
+
+
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
+from resizeimage import resizeimage
+
 
 # Create your models here.
+class QuoteTable(models.Model):
+    quote=models.TextField(max_length=300,null=False,blank=False)
+    qauthor=models.CharField(max_length=150,null=True,blank=True)
+    type_choice=[('Motivational','Motivational'),('Success','success'),('Friendship','Friendship'),('Inspirational','Inspirational'),('Positive','Positive'),('Life','Life'),('Wisdom','Wisdom'),('Faith','Faith'),('Freedom','Freedom')]
+    qtype=models.CharField(max_length=20,choices=type_choice,default='Motivational',)
+    qimage=models.ImageField(upload_to="uploaded")
+   
+    tags=TaggableManager()
+
+
+
+
+    def save(self,*args,**kwargs):
+        img=Image.open(self.qimage)
+        new_img=resizeimage.resize_cover(img,[300,300])
+        new_img_io=BytesIO()
+        new_img.save(new_img_io,new_img.format)
+        temp_name=self.qimage.name
+        self.qimage.delete(save=False)
+
+        self.qimage.save(temp_name,content=ContentFile(new_img_io.getvalue()),save=False)
+        
+        super(QuoteTable,self).save(*args,**kwargs)
+
+
+
+   
+    def __str__(self):
+        return self.qauthor
+        
+    def get_absolute_url(self):
+        return reverse("list", kwargs={"pk": self.pk})
+        
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,83 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from requests import request
+from myapp.models import QuoteTable
+from django.urls import reverse_lazy
+from myapp.forms import QuoteForm
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 # Create your views here.
-def home(request):
-    return render(request,'myapp/base.html')
+
+class QuoteHomeView(ListView):
+    model=QuoteTable
+    template_name='myapp/list.html'
+    context_object_name='quotes'
+
+class QuoteTagListView(QuoteHomeView):
+    template_name='myapp/taglist.html'
+    def get_tag(self):
+        return self.kwargs.get('tag')
+    def get_queryset(self):
+        return self.model.objects.filter(tags__slug=self.get_tag())
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["tag"]=self.get_tag()
+        return context
+class QuoteDetailView(DetailView):
+    model=QuoteTable
+    template_name='myapp/detail.html'
+    context_object_name='quotes'
+
+# @method_decorator(login_required,name='form_valid')
+class QuoteCreateView(LoginRequiredMixin,CreateView):
+    model=QuoteTable
+    fields=['quote','qauthor','qtype','qimage','tags']
+    template_name='myapp/create.html'
+    success_url=reverse_lazy('quotes:list')
+    
+    def form_valid(self,form):
+        form.instance.user=self.request.user
+        return super().form_valid(form)
+class UserIsSubmitter(UserPassesTestMixin):
+    def get_quotes(self):
+        return get_object_or_404(QuoteTable,pk=self.kwargs.get('pk'))
+    def test_func(self):
+        if self.request.user.is_authenticated:
+            return self.request.user==self.get_quotes().username
+        else:
+            raise PermissionDenied('Sorry Your are not allowed to upload')
+class QuoteUpdateView(UpdateView):
+    
+    model=QuoteTable
+    # fields=['quote','qauthor','qtype','qimage','tags']
+    fields='__all__'
+    template_name='myapp/update.html'
+    success_url=reverse_lazy('quotes:list')
+class QuoteDeleteView(DeleteView):
+    model=QuoteTable
+    # template_name='myapp/delete.html'
+    success_url=reverse_lazy('quotes:list')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
